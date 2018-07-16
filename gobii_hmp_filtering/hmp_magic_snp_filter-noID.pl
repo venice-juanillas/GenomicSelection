20bff83a2d7d052b90b55e04d98a773213903b90
Venice Juanillas <v.juanillas@irri.org>
2018-07-16 09:09:44 +0000

    Add filtering tool

---
+++ gobii_hmp_filtering/hmp_magic_snp_filter-noID.pl
@@ -0,0 +1,131 @@
+#!/usr/bin/perl
+# hapmap parser according to Chitra's specs
+# filter out SNPs based on user-selected criteria - FOR LINES & founders
+# RP Mauleon 10Dec 2013
+
+use warnings;
+use List::Util qw( min max );  # to get min and max values in an array
+
+my ($line, $taxa,$numTaxa, $ref,$missingCutoff);
+my $ctA = $ctT = $ctC = $ctG = $monoCt =0;
+my $ctMissing =0; ## count of N for each SNP
+my $validSampCut = 0.25 ; ## how much samples should be allowed to have missing SNP calls, for founders at least 2/8
+my $maf = 0.125 ; ## minor allele frequency allowed ... optimized for 8 founders (at least 1/8 poly)
+my $maxAF =1; #max allele frequency is 1
+my @afs = () ; ## store NT freqs for each SNP, empty array...
+my ($fA, $fT, $fC, $fG, $minAF); #allele freqs of each NT, minimum allele frequency not zero, 
+my (@line,@taxa, @filteredLines);
+my $het_cutoff = 0.8;
+
+if(!@ARGV){
+	die " $! \nUsage: ./script <hapmap_file> <outputFile> <allowed-proportion-taxa-with-missing-SNPs> <MAF_cutoff> <incl_monomorphic> <hets_cutoff> \n";
+}
+
+
+my $infile; ## main data file is 1st parameter passed
+
+$infile = $ARGV[0]; ## data file
+my $outfile = $infile . "filtered"; ## automatic output filename ...
+
+if ($ARGV[1]) { ## outputFilename specified
+	$outfile =$ARGV[1]; 
+} ## end if ARGV1
+
+if ($ARGV[2]) { ## proportion of samples with missing SNP state allowed (default=0.25)
+	$validSampCut =$ARGV[2]; 
+} ## end if ARGV2
+
+if ($ARGV[3]) { ## minor allele frequency
+	$maf =$ARGV[3]; 
+} ## end if ARGV3
+
+if ($ARGV[4] == 1) { ## include monomorphic SNPs in dataset? boolean... 1=yes, any other = no
+	$maxAF = 1.5; 
+} ## end if ARGV4
+
+if()
+
+open IN, "$infile"; # or die $!;
+open OUT, ">$outfile"; ## parsed file output name
+
+
+while (<IN>) {
+	$line = $_;
+	@line = split "\t", $line;
+	$numTaxa = scalar(@line) - 12 ; ## compute number of taxa in data, the 1st 13 columns are NOT included
+
+	if ($line =~ m/rs\#/) { ## 1st line
+		print OUT $line;
+		next;
+	} ## end print 1st line
+
+	for (my $i = 11; $i < scalar(@line); $i++) { ## loop thru each array element (taxa) starting from 12...
+			## some code to filter....
+		## COUNT Ns
+		if ($line[$i] =~ "N") {
+			$ctMissing ++;  ## increment count of N
+		} ## end ct N
+		## COUNT NT calls
+		if ($line[$i] eq "A") {
+			$ctA ++;  ## increment count of A
+		} ## end ct A...
+		if ($line[$i] eq "T") {
+			$ctT ++;  ## increment count of T
+		} ## end ct T...
+		if ($line[$i] eq "C") {
+			$ctC ++;  ## increment count of C
+		} ## end ct C...
+		if ($line[$i] eq "G") {
+			$ctG ++;  ## increment count of G
+		} ## end ct G...
+
+		
+	} ## end for i
+	##print "$numTaxa\t$ctMissing\n";
+	
+	## CONDITIONAL printing of taxa lines ....
+	$missingCutoff = sprintf("%0d", ($validSampCut * $numTaxa)); ## round off , this is the max # allowed to be missing...
+	#$missingCutoff =($validSampCut * $numTaxa); ## round off , this is the max # allowed to be missing...
+	$monoCt = $numTaxa - $ctMissing;  ## the number of observed non-N taxa
+	if ($monoCt == 0) {
+		$monoCt = 0.001 ; ## avoid div/0 error saka 
+	} ## error handling...
+
+	##compute NT frequencies
+	$fA = $ctA/$monoCt;
+	$fT = $ctT/$monoCt;
+	$fC = $ctC/$monoCt;
+	$fG = $ctG/$monoCt;
+	#print "$fA\t$fT\t$fC\t$fG\n";
+	
+	push (@afs,  $fA) if $fA >0;
+	push (@afs,  $fT) if $fT >0;
+	push (@afs,  $fC) if $fC >0;
+	push (@afs,  $fG) if $fG >0;
+	$minAF = min @afs;  ## get the lowest number in this array - this is the min allele frequency 
+	#print "$minAF\n";
+
+	
+	if ($ctMissing <= $missingCutoff ) {  ## Count of missing is less than maximum allowed...
+		if ($minAF >= $maf && $minAF < $maxAF ) {  ## least frequent allele is > than MAF but not mono --> minAF = 0 
+			print OUT $line;
+		}
+	} ## end if ctmissing < = missing cutoff
+	
+	
+	#print "$monoCt \t $ctA \t $ctT \t $ctC \t $ctG \n";
+	## reset the counters....
+	$ctMissing = 0; ## reset
+	$ctA = 0;
+	$ctT = 0;
+	$ctC = 0;
+	$ctG = 0;
+	@afs = ();
+
+	
+} ## end while IN
+
+close IN;
+close OUT;
+
+
