5601988f57313ea37c974d06880df2dbe118a972
Venice Juanillas <v.juanillas@irri.org>
2018-07-18 03:39:25 +0000

    Modify filtering tool to accept GOBII hapmap format

--- gobii_hmp_filtering/hmp_magic_snp_filter-noID.pl
+++ gobii_hmp_filtering/hmp_magic_snp_filter-noID.pl
@@ -3,11 +3,15 @@
 # filter out SNPs based on user-selected criteria - FOR LINES & founders
 # RP Mauleon 10Dec 2013
 
+# modified to accept GOBII-hapmap format
+# Edit: VJuanillas (v.juanillas@irri.org
+# Date: July 18, 2018
+
 use warnings;
 use List::Util qw( min max );  # to get min and max values in an array
 
 my ($line, $taxa,$numTaxa, $ref,$missingCutoff);
-my $ctA = $ctT = $ctC = $ctG = $monoCt =0;
+my $ctA = $ctT = $ctC = $ctG = $monoCt = $het =0;
 my $ctMissing =0; ## count of N for each SNP
 my $validSampCut = 0.25 ; ## how much samples should be allowed to have missing SNP calls, for founders at least 2/8
 my $maf = 0.125 ; ## minor allele frequency allowed ... optimized for 8 founders (at least 1/8 poly)
@@ -15,10 +19,10 @@ my $maxAF =1; #max allele frequency is 1
 my @afs = () ; ## store NT freqs for each SNP, empty array...
 my ($fA, $fT, $fC, $fG, $minAF); #allele freqs of each NT, minimum allele frequency not zero, 
 my (@line,@taxa, @filteredLines);
-my $het_cutoff = 0.8;
+my $het_cutoff = 0.8;	
 
 if(!@ARGV){
-	die " $! \nUsage: ./script <hapmap_file> <outputFile> <allowed-proportion-taxa-with-missing-SNPs> <MAF_cutoff> <incl_monomorphic> <hets_cutoff> \n";
+	die " $! \nUsage: ./script <hapmap_file> <outputFile> <allowed-proportion-taxa-with-missing-SNPs> <MAF_cutoff> <incl_monomorphic> <hets_cutoff>\n";
 }
 
 
@@ -43,7 +47,9 @@ if ($ARGV[4] == 1) { ## include monomorphic SNPs in dataset? boolean... 1=yes, a
 	$maxAF = 1.5; 
 } ## end if ARGV4
 
-if()
+if($ARGV[5]){ ## het cut-off threshold
+	$het_cutoff = $ARGV[5]; 
+}
 
 open IN, "$infile"; # or die $!;
 open OUT, ">$outfile"; ## parsed file output name
@@ -53,6 +59,11 @@ while (<IN>) {
 	$line = $_;
 	@line = split "\t", $line;
 	$numTaxa = scalar(@line) - 12 ; ## compute number of taxa in data, the 1st 13 columns are NOT included
+	
+	if($line =~ m/^\#/){
+		print OUT $line;
+		next;
+	}
 
 	if ($line =~ m/rs\#/) { ## 1st line
 		print OUT $line;
@@ -62,26 +73,31 @@ while (<IN>) {
 	for (my $i = 11; $i < scalar(@line); $i++) { ## loop thru each array element (taxa) starting from 12...
 			## some code to filter....
 		## COUNT Ns
-		if ($line[$i] =~ "N") {
+		if ($line[$i] =~ "NN") {
 			$ctMissing ++;  ## increment count of N
 		} ## end ct N
 		## COUNT NT calls
-		if ($line[$i] eq "A") {
+		elsif ($line[$i] eq "AA") {
 			$ctA ++;  ## increment count of A
 		} ## end ct A...
-		if ($line[$i] eq "T") {
+		elsif ($line[$i] eq "TT") {
 			$ctT ++;  ## increment count of T
 		} ## end ct T...
-		if ($line[$i] eq "C") {
+		elsif ($line[$i] eq "CC") {
 			$ctC ++;  ## increment count of C
 		} ## end ct C...
-		if ($line[$i] eq "G") {
+		elsif ($line[$i] eq "GG") {
 			$ctG ++;  ## increment count of G
 		} ## end ct G...
-
+		else{ # else
+			$het ++;
+		}
+		
 		
 	} ## end for i
 	##print "$numTaxa\t$ctMissing\n";
+	
+	print "Het: $het\n";
 	
 	## CONDITIONAL printing of taxa lines ....
 	$missingCutoff = sprintf("%0d", ($validSampCut * $numTaxa)); ## round off , this is the max # allowed to be missing...
